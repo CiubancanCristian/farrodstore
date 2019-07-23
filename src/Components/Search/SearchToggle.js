8abe660d6cda55544c7fc3c96d4dd4b9cf55ed0d
CiubancanCristian <cristian@ciubancan.com>
2019-07-23 18:41:41 +0300

    second take after bug

--- src/Components/Search/SearchToggle.js
+++ src/Components/Search/SearchToggle.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./SearchToggle.css";
+import magnifier from "./magnifier.png"
 
 const SearchToggle = props => (
 	<input className="search" onClick={props.click} type="image" src={magnifier} alt="Submit" width="48" height="48"/>
